350c2d7eb05fa9d9f679595d35c27b42f1c3176d
Ali Zia <ziaali455@gmail.com>
2023-07-23 21:11:04 -0400

    Updates

---
+++ precache-manifest.fc354b3bf0f8100385b3aeca2acc3809.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e42c43c262ff08c02d6b",
+    "url": "/nightsinpinheiros/static/css/main.c3559413.chunk.css"
+  },
+  {
+    "revision": "e42c43c262ff08c02d6b",
+    "url": "/nightsinpinheiros/static/js/main.e42c43c2.chunk.js"
+  },
+  {
+    "revision": "93c748a80b187c4a0720",
+    "url": "/nightsinpinheiros/static/js/1.93c748a8.chunk.js"
+  },
+  {
+    "revision": "ab59db5adf44356c5134",
+    "url": "/nightsinpinheiros/static/css/2.1e47550a.chunk.css"
+  },
+  {
+    "revision": "ab59db5adf44356c5134",
+    "url": "/nightsinpinheiros/static/js/2.ab59db5a.chunk.js"
+  },
+  {
+    "revision": "ec127d962928861fe68a",
+    "url": "/nightsinpinheiros/static/js/runtime~main.ec127d96.js"
+  },
+  {
+    "revision": "8dcc6b5262f3b6138b1566b357ba89a9",
+    "url": "/nightsinpinheiros/static/media/github-mark.8dcc6b52.svg"
+  },
+  {
+    "revision": "7f95d8eb2f918e816947ca574b963865",
+    "url": "/nightsinpinheiros/index.html"
+  }
+];
